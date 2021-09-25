fe9b596aded2623eab24a34ada718661422f2a21
Silvano Costanzo <scostanzo.dev@gmail.com>
2021-09-25 15:43:35 +0200

    n4s in progress

---
+++ go/need-for-speed/need_for_speed.go
@@ -0,0 +1,35 @@
+package speed
+
+// TODO: define the 'Car' type struct
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{batteryDrain: batteryDrain, speed: speed, battery: 100}
+}
+
+// TODO: define the 'Track' type struct
+type Track struct {
+	distance int
+}
+
+// NewTrack created a new track
+func NewTrack(distance int) Track {
+	return Track{distance: distance}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive on more time,
+// the car will not move.
+func Drive(car Car) Car {
+
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	panic("Please implement the CanFinish function")
+}
