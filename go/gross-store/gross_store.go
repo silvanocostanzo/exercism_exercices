2c4caaf957c4b24d15c2a19bf37185e082c408cc
Silvano Costanzo <scostanzo.dev@gmail.com>
2021-10-19 15:24:10 +0200

    [GO] adding gross-store solution

--- go/gross-store/gross_store.go
+++ go/gross-store/gross_store.go
@@ -19,15 +19,41 @@ func NewBill() map[string]int {
 
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
-
+	if v, ok := units[unit]; ok {
+		bill[item] = v
+		return true
+	}
+	return false
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the RemoveItem() function")
+	if _, ok := bill[item]; !ok {
+		return false
+	}
+
+	if _, ok := units[unit]; !ok {
+		return false
+	}
+
+	newUnit := bill[item] - units[unit]
+	switch {
+	case newUnit < 0:
+		return false
+	case newUnit == 0:
+		delete(bill, item)
+	default:
+		bill[item] -= units[unit]
+	}
+
+	return true
+
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
-	panic("Please implement the GetItem() function")
+	if _, ok := bill[item]; !ok {
+		return 0, false
+	}
+	return bill[item], true
 }
