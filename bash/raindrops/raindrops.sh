beb1abb30390a38c95512ea91a2fc0b8e4c29fc7
Silvano Costanzo <silvano.costanzo@akeneo.com>
2021-12-23 09:14:07 +0100

    BASH: Raindrops solution

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,47 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+FACTOR_THREE=$(($1 % 3 == 0))
+FACTOR_FIVE=$(($1 % 5 == 0))
+FACTOR_SEVEN=$(($1 % 7 == 0))
 
-arg=$1
-echo $(($1 % 2 == 0))
-
-
-case $1 in
-
-  3)
-    echo "Pling"
-    ;;
-
-  5)
-    echo "Plang"
-    ;;
-
-  7)
-    echo "Plong"
-    ;;
-
-  *)
+if [[ $FACTOR_THREE == 0 && $FACTOR_FIVE == 0 && $FACTOR_SEVEN == 0 ]]; then
     echo $1
-    ;;
-esac
+    exit 0
+fi
+
+
+string=""
+
+if [[ $FACTOR_THREE == 1 ]]; then
+    string+="Pling"
+fi
+
+if [[ $FACTOR_FIVE == 1 ]]; then
+    string+="Plang"
+fi
+
+if [[ $FACTOR_SEVEN == 1 ]]; then
+    string+="Plong"
+fi
+
+echo $string
