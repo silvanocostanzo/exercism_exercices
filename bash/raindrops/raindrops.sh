fdb96901c4f564059cdb81478f87a9ab5562b6e8
Silvano Costanzo <silvano.costanzo@akeneo.com>
2021-12-23 20:59:08 +0100

    bash: better raindrops solution

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-FACTOR_THREE=$(($1 % 3 == 0))
-FACTOR_FIVE=$(($1 % 5 == 0))
-FACTOR_SEVEN=$(($1 % 7 == 0))
-
-if [[ $FACTOR_THREE == 0 && $FACTOR_FIVE == 0 && $FACTOR_SEVEN == 0 ]]; then
-    echo $1
-    exit 0
-fi
-
-
 string=""
 
-if [[ $FACTOR_THREE == 1 ]]; then
+if [[ $(expr $1 % 3) == 0 ]]; then
     string+="Pling"
 fi
 
-if [[ $FACTOR_FIVE == 1 ]]; then
+if [[ $(expr $1 % 5) == 0 ]]; then
     string+="Plang"
 fi
 
-if [[ $FACTOR_SEVEN == 1 ]]; then
+if [[ $(expr $1 % 7) == 0 ]]; then
     string+="Plong"
+fi
+
+if [[ -z $string ]]; then
+    string=$1
 fi
 
 echo $string
