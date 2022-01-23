55d32f66891b3ff3959f0a751de8d2e036732440
Silvano Costanzo <scostanzo.dev@gmail.com>
2022-01-23 22:04:22 +0100

    GO: bird watcher in progress

---
+++ go/bird-watcher/bird_watcher.go
@@ -0,0 +1,30 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	counter := 0
+	for _, v := range birdsPerDay {
+		counter += v
+	}
+	return counter
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	counter := 0
+	var givenWeek []int
+	i := 7 * (week - 1)
+	givenWeek = birdsPerDay[i : i+7]
+	for i := 0; i < len(givenWeek); i++ {
+		counter += givenWeek[i]
+	}
+	return counter
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	return []int{}
+}
