92e06f4340846dc192e4b6673653f40d6f5ae7a8
Silvano Costanzo <scostanzo.dev@gmail.com>
2022-01-23 12:04:32 +0100

    GO: time exercise: naive solution

--- go/booking-up-for-beauty/booking_up_for_beauty.go
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -1,13 +1,15 @@
 package booking
 
 import (
+	"fmt"
 	"log"
 	"time"
 )
 
 // Schedule returns a time.Time from a string containing a date
 func Schedule(date string) time.Time {
-	t, err := time.Parse("1/02/2006 15:04:05", date)
+	layout := "1/02/2006 15:04:05"
+	t, err := time.Parse(layout, date)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -16,22 +18,49 @@ func Schedule(date string) time.Time {
 
 // HasPassed returns whether a date has passed
 func HasPassed(date string) bool {
-	now := time.Now()
-	d := Schedule(date)
-	return d.After(now)
+	layout := "January 2, 2006 15:04:05"
+	t, err := time.Parse(layout, date)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return time.Now().After(t)
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon
 func IsAfternoonAppointment(date string) bool {
-	panic("Please implement the IsAfternoonAppointment function")
+	layout := "Monday, January 2, 2006 15:04:05"
+	t, err := time.Parse(layout, date)
+	if err != nil {
+		log.Fatal(err)
+	}
+	hour := t.Hour()
+	return hour < 20 && hour > 11
 }
 
 // Description returns a formatted string of the appointment time
 func Description(date string) string {
-	panic("Please implement the Description function")
+	layout := "1/2/2006 15:04:05"
+	t, err := time.Parse(layout, date)
+	if err != nil {
+		log.Fatal(err)
+	}
+	weekDay := t.Weekday().String()
+	month := t.Month().String()
+	day := t.Day()
+	year := t.Year()
+	hour := t.Hour()
+	minutes := t.Minute()
+
+	return fmt.Sprintf("You have an appointment on %s, %s %d, %d, at %d:%d.", weekDay, month, day, year, hour, minutes)
 }
 
 // AnniversaryDate returns a Time with this year's anniversary
 func AnniversaryDate() time.Time {
-	panic("Please implement the AnniversaryDate function")
+	layout := "2006/01/02 15:04:05"
+	date := "2022/09/15 00:00:00"
+	t, err := time.Parse(layout, date)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return t
 }
